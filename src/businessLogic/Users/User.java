8850c8631c3f7d09bb2ba2bf72dd1c6cac9faaeb
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-03-06 21:04:05 -0500

    Update User.java

--- src/businessLogic/Users/User.java
+++ src/businessLogic/Users/User.java
@@ -1,5 +1,13 @@
 package businessLogic.Users;
 
 public class User {
+  private String name;
+  private String pw;
+//basic constructor
+  public User(String name, String pw){
+    this.name=name;
+    this.pw=pw;
+  }
 
+  
 }
