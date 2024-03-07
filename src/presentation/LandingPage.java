ee29ff5f8746e7ac626fc04d4ef8416ccd7a595d
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-03-06 22:07:20 -0500

    Create LandingPage.java

---
+++ src/presentation/LandingPage.java
@@ -0,0 +1,34 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
+import javax.swing.JPasswordField;
+import javax.swing.SwingUtilities;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.net.URL;
+import java.awt.event.ActionEvent;
+
+public class LandingPage{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+}
