8b189a27d7d12592fbf532e55fe85a021e96275f
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-03-06 21:10:11 -0500

    Update Faculty.java

--- src/businessLogic/Users/Faculty.java
+++ src/businessLogic/Users/Faculty.java
@@ -5,6 +5,16 @@ public class Faculty extends User {
   public Faculty(String name, String pw){
     super(name, pw);
   }
+
+  //mutators and accessors
+  public String getName{
+    return name;
+  }
+
+  public String getPW(){
+    return pw;
+  }
+
   
 }
 
