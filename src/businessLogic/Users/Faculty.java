c0b040d8c059a4c35159cbdcf6c9183170db27ae
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-03-06 21:43:12 -0500

    Update Faculty.java

--- src/businessLogic/Users/Faculty.java
+++ src/businessLogic/Users/Faculty.java
@@ -1,9 +1,11 @@
 package businessLogic.Users;
 
 public class Faculty extends User {
-
-  public Faculty(String name, String pw){
+private int fID;
+  //simple constructor with super(), keep private for builder pattern to function correctly
+ Faculty(String name, String pw){
     super(name, pw);
+    this.fID=fID;
   }
 
 
@@ -11,7 +13,27 @@ public class Faculty extends User {
 }
 
 //Implementing builder pattern
-public static class FacBuilder{
+public class FacBuilder{
+  private String name;
+  private String pw;
+  private int fID;
 
+  //builder setters
+  public FacBuilder name(String name){
+    this.name=name;
+  }
+
+  public FacBuilder pw(String pw){
+    this.pw=pw;
+  }
+
+  public FacBuilder fID(int fID){
+    this.fID=fID;
+  }
+
+  //build method
+  public Faculty build(){
+    return new Faculty(name, pw, fID);
+  }
   
 }
