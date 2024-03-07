2b2cb34fe26ff17a4fcdda635ca10789e50ce239
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-03-06 21:02:46 -0500

    Update Faculty.java

--- src/businessLogic/Users/Faculty.java
+++ src/businessLogic/Users/Faculty.java
@@ -2,4 +2,13 @@ package businessLogic.Users;
 
 public class Faculty extends User {
 
+  public Faculty(String name, String pw){
+    super(name, pw);
+  }
+  
+}
+
+public static class FacBuilder{
+
+  
 }
