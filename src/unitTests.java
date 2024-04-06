cc0415f4bbbe9cd3e15e347cf05094b8e7fa348c
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-04-05 22:33:00 -0400

    Update unitTests.java

--- src/unitTests.java
+++ src/unitTests.java
@@ -14,5 +14,34 @@ String name1 = a1.getName();
 AssertEquals("FacultyName", name1);
 }
 
+  //Student Testing
+@Test
+public void test(){
+  //test accessors
+User a2 = new Student();
+Builder name(studentName);
+String name2 = a2.getName();
+AssertEquals("studentName", name2);
+}
+
+  //Guest Testing
+@Test
+public void test(){
+  //test accessors
+User a3 = new Guest();
+Builder name(guestName);
+String name3 = a3.getName();
+AssertEquals("guestName", name3);
+}
+
+  //NonFac Testing
+  @Test
+public void test(){
+  //test accessors
+User a4 = new NonFaculty();
+Builder name(nonFacName);
+String name4 = a4.getName();
+AssertEquals("nonFacName", name4);
+}
   
 }
