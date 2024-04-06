8ea85181519828ffcf37eab1002f90ea4b1be919
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-04-05 22:16:31 -0400

    Update unitTests.java

--- src/unitTests.java
+++ src/unitTests.java
@@ -7,8 +7,11 @@ public class unitTests{
   //Faculty Testing
 @Test
 public void test(){
+  //test accessors
 User a1 = new Faculty();
-  
+Builder name(FacultyName);
+String name1 = a1.getName();
+AssertEquals("FacultyName", name1);
 }
 
   
