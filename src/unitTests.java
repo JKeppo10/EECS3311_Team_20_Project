b2895d41554f8855867793ee608240a3f4094247
Md <90801074+MdIshmam1@users.noreply.github.com>
2024-04-05 22:50:17 -0400

    Update unitTests.java

--- src/unitTests.java
+++ src/unitTests.java
@@ -9,17 +9,25 @@ public class unitTests{
 public void test(){
   //test accessors
 User a1 = new Faculty();
-Builder name(FacultyName);
+Builder name("FacultyName");
 String name1 = a1.getName();
 AssertEquals("FacultyName", name1);
 }
+
+  @Test
+  public void test(){
+   //general object creation for Faculty user; dynamic polymorphism 
+  User a1a = new Faculty("Bob", "password1", 1111, "faculty1@yorku.ca", FACULTY, 2);
+    //test Factory design pattern implementation
+  
+  }
 
   //Student Testing
 @Test
 public void test(){
   //test accessors
 User a2 = new Student();
-Builder name(studentName);
+Builder name("studentName");
 String name2 = a2.getName();
 AssertEquals("studentName", name2);
 }
@@ -29,7 +37,7 @@ AssertEquals("studentName", name2);
 public void test(){
   //test accessors
 User a3 = new Guest();
-Builder name(guestName);
+Builder name("guestName");
 String name3 = a3.getName();
 AssertEquals("guestName", name3);
 }
@@ -39,7 +47,7 @@ AssertEquals("guestName", name3);
 public void test(){
   //test accessors
 User a4 = new NonFaculty();
-Builder name(nonFacName);
+Builder name("nonFacName");
 String name4 = a4.getName();
 AssertEquals("nonFacName", name4);
 }
